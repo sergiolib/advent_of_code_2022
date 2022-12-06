5168394e95530b92fb2efa72469d72e12e94733e
Sergio Liberman Bronfman <sergiolib@gmail.com>
2022-12-06 21:05:51 +0100

    Implemented solutions for day 6

---
+++ aoc6_2/src/main.rs
@@ -0,0 +1,29 @@
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_path = &args[1];
+    println!("Reading {}", file_path);
+    let text = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    let s = 14;
+    let mut msg: Vec<char> = "a".repeat(s).chars().collect();
+    let mut pos = 0;
+    for (in_pos, c) in text.char_indices() {
+        msg[in_pos % s] = c;
+        pos = in_pos;
+        if in_pos > (s - 1) {
+            let mut smsg = msg.clone();
+            smsg.sort_unstable();
+            smsg.dedup();
+            // println!("smsg: {}", String::from_iter(&smsg));
+
+            if smsg.len() == s {
+                break;
+            }
+        }
+        // println!("{}", String::from_iter(&msg));
+    }
+    println!("{}", pos + 1);
+}
